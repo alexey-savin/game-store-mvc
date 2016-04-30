e13a25a77cadb01b7f60ed216bf8d6c42d2003f9
alexey-savin <alexeyv.savin@gmail.com>
2016-04-30 14:26:58 +0300

    - added entity - repository abstraction - mock repository

---
+++ GameStore/GameStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Moq;
+using Ninject;
+using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+
+namespace GameStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+
+        private void AddBindings()
+        {
+            Mock<IGameRepository> mock = new Mock<IGameRepository>();
+            mock.Setup(m => m.Games)
+                .Returns(new List<Game>
+                {
+                    new Game { Name = "Sym City", Price = 1499 },
+                    new Game { Name = "TITANFALL", Price = 2299 },
+                    new Game { Name = "Battlefield 4", Price = 899.9M }
+                });
+
+            kernel.Bind<IGameRepository>().ToConstant(mock.Object);
+        }
+
+        public object GetService(Type serviceType) 
+            => kernel.TryGet(serviceType);
+
+        public IEnumerable<object> GetServices(Type serviceType)
+            => kernel.GetAll(serviceType);
+    }
+}
