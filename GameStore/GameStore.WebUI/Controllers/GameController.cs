fffd7524344a3ae438bb2c920e88429c5887cb5d
alexey-savin <alexeyv.savin@gmail.com>
2016-04-30 14:45:46 +0300

    - added view

---
+++ GameStore/GameStore.WebUI/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class GameController : Controller
+    {
+        private IGameRepository _repository;
+
+        public GameController(IGameRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public ViewResult List()
+            => View(_repository.Games);
+    }
+}
