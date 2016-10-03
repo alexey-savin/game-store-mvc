89dffe9a48040da433c6fefb6c0d56b9e58b5e3e
alexey-savin <alexeyv.savin@gmail.com>
2016-10-03 12:32:16 +0300

    cart controller created

---
+++ GameStore/GameStore.WebUI/Controllers/CartController.cs
@@ -0,0 +1,19 @@
+﻿using GameStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStore.WebUI.Controllers
+{
+    public class CartController : Controller
+    {
+        private IGameRepository _repository;
+
+        public CartController(IGameRepository repo)
+        {
+            _repository = repo;
+        }
+    }
+}
