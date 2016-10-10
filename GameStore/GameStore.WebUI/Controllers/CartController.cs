da82461fd9d0b78a78f235813f18285097926b91
alexey-savin <alexeyv.savin@gmail.com>
2016-10-10 12:01:20 +0300

    cart view add to cart

--- GameStore/GameStore.WebUI/Controllers/CartController.cs
+++ GameStore/GameStore.WebUI/Controllers/CartController.cs
@@ -1,4 +1,6 @@
 ﻿using GameStore.Domain.Abstract;
+using GameStore.Domain.Entities;
+using GameStore.WebUI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,54 @@ namespace GameStore.WebUI.Controllers
         public CartController(IGameRepository repo)
         {
             _repository = repo;
+        }
+
+        public ViewResult Index(string returnUrl)
+        {
+            return View(new CartIndexViewModel
+            {
+                Cart = GetCart(),
+                ReturnUrl = returnUrl
+            });
+        }
+
+        public RedirectToRouteResult AddToCart(int gameId, string returnUrl)
+        {
+            Game game = _repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                GetCart().AddItem(game, 1);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public RedirectToRouteResult RemoveFromCart(int gameId, string returnUrl)
+        {
+            Game game = _repository.Games
+                .FirstOrDefault(g => g.GameId == gameId);
+
+            if (game != null)
+            {
+                GetCart().RemoveLine(game);
+            }
+
+            return RedirectToAction("Index", new { returnUrl });
+        }
+
+        public Cart GetCart()
+        {
+            Cart cart = (Cart)Session["Cart"];
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                Session["Cart"] = cart;
+            }
+
+            return cart;
         }
     }
 }
